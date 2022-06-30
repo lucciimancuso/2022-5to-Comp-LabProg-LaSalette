b334c7f779d73d05a276eb26454bccf964c35d6f
Luccii <101129180+lucciimancuso@users.noreply.github.com>
2022-06-30 14:16:00 -0300

    Recuperatorio

---
+++ Lavadora.java
@@ -0,0 +1,55 @@
+
+package lavadora;
+
+public class Lavadora {
+
+ 
+public static void main(String[] args) {
+        
+    }
+    
+public class Lavadora extends Electrodomestico{
+  
+  
+    private final static int CARGA_DEF=5;
+  
+ 
+    private int carga;
+  
+    
+    public int getCarga() {
+        return carga;
+    }
+  
+ 
+    public double precioFinal(){
+        
+        double plus=super.precioFinal();
+  
+        
+        if (carga>30){
+            plus+=50;
+        }
+  
+        return plus;
+    }
+  
+
+    public Lavadora(){
+        this(PRECIO_BASE_DEF, PESO_DEF, CONSUMO_ENERGETICO_DEF, COLOR_DEF, CARGA_DEF);
+    }
+  
+ 
+    public Lavadora(double precioBase, double peso){
+        this(precioBase, peso, CONSUMO_ENERGETICO_DEF, COLOR_DEF, CARGA_DEF);
+    }
+  
+ 
+    public Lavadora(double precioBase, double peso, char consumoEnergetico, String color, int carga){
+        super(precioBase,peso, consumoEnergetico,color);
+        this.carga=carga;
+    }
+}
+
+
+}
